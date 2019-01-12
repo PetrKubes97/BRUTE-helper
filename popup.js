63789dd933a15d5ea294990cd0f7da8adcdfdfa6
Petr Kubeš <petrkubes1997@gmail.com>
2019-01-12 11:39:16 +0100

    added clear button

--- popup.js
+++ popup.js
@@ -1,10 +1,9 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
 window.onload = function () {
+  const table = document.getElementById("content");
+  const th = `<tr><th style="text-align:left;">Url</th><th style="text-align:right;">Datum</th></tr>`;
+
   chrome.storage.sync.get(['urls'], function (result) {
     let urls;
     if (result.urls == undefined) {
@@ -13,12 +12,20 @@ window.onload = function () {
       urls = JSON.parse(result.urls);
     }
 
-    const table = document.getElementById("content");
-
+    table.innerHTML = th;
     urls.reverse().forEach(urlItem => {
       const str = `<tr><td><a href=\"${urlItem.url}\" target=\"_blank\">${urlItem.url}</a></td><td style =\"text-align:right; min-width:190px;\">${urlItem.date}</td></tr>`;
       table.insertAdjacentHTML('beforeend', str);
     });
 
   });
+
+  document.getElementById("clear").addEventListener("click", function () {
+    const r = confirm("Všechny BRUTE adresy budou smazány. Jsi si jistý/á?");
+    if (r) {
+      chrome.storage.sync.set({ urls: null }, function (res) {
+        table.innerHTML = th;
+      });
+    };
+  })
 }
